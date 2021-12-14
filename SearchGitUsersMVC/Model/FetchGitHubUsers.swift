80fb6dbc90210eb33f9a8c98394d4bfe60986fe8
sauna1137 <subscription.search@gmail.com>
2021-12-14 21:58:14 +0900

    プロトコル、アラート追加
    アラートをVCへ渡すプロトコル

--- SearchGitUsersMVC/Model/FetchGitHubUsers.swift
+++ SearchGitUsersMVC/Model/FetchGitHubUsers.swift
@@ -8,8 +8,13 @@
 import Foundation
 import Alamofire
 
+protocol FetchGitHubUsersDelegate: AnyObject {
+    func presentAlert(content: String)
+}
+
 class FetchGitHubUsers {
 
+    weak var fetchGitHubUsersDelegate: FetchGitHubUsersDelegate?
     private let gitHubAPIURL = "https://api.github.com/search/users?q=" // ユーザー情報を取得するAPI q=以下にキーワード
     private var gitHubUsers = [GitHubUsers]()
     private let maxContents = 30 // APIの構造上30件以上取得できないため
@@ -17,13 +22,23 @@ class FetchGitHubUsers {
     // SearchVCで検索キーワードを元にAPIを叩いて値を返す
     func fetchGitHubUsers(searchText: String, completion: @escaping ([GitHubUsers]) -> Void) {
 
+        gitHubUsers.removeAll()
+
         AF.request(gitHubAPIURL + searchText) { _ in } .response { response in
 
-            guard let data = response.data else { return }
+            guard let data = response.data else {
+                self.fetchGitHubUsersDelegate?.presentAlert(content: "ユーザーは見つかりませんでした。")
+                return
+            }
 
             do {
                 let json = try JSONDecoder().decode(CodableForGitHubUsers.self, from: data)
                 var totalCount = json.totalCount
+
+                guard totalCount > 0 else {
+                    self.fetchGitHubUsersDelegate?.presentAlert(content: "ユーザーは見つかりませんでした。")
+                    return
+                }
 
                 if totalCount > self.maxContents {
                     totalCount = self.maxContents
@@ -41,7 +56,10 @@ class FetchGitHubUsers {
 
                 completion(self.gitHubUsers)
 
-            } catch { print(error.localizedDescription) }
+            } catch {
+                print(error.localizedDescription)
+                self.fetchGitHubUsersDelegate?.presentAlert(content: "予期せぬエラーが発生しました")
+            }
         }
     }
 }
