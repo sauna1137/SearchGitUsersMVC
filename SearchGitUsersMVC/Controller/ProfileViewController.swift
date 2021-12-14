dbacab3ae68757da183f98e2e0d7cacfb5b72bfb
sauna1137 <subscription.search@gmail.com>
2021-12-14 16:24:49 +0900

    ProfileVC追加　first Commit

---
+++ SearchGitUsersMVC/Controller/ProfileViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ProfileViewController.swift
+//  SearchGitUsersMVC
+//
+//  Created by KS on 2021/12/13.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
