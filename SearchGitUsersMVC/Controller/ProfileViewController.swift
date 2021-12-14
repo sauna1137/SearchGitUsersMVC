eec5440fb8f216e672d8a305268184ee284725d6
sauna1137 <subscription.search@gmail.com>
2021-12-14 21:54:01 +0900

    WebKit追加　WebView、Web表示完成

--- SearchGitUsersMVC/Controller/ProfileViewController.swift
+++ SearchGitUsersMVC/Controller/ProfileViewController.swift
@@ -6,9 +6,27 @@
 //
 
 import UIKit
+import WebKit
 
 class ProfileViewController: UIViewController {
+
+    private var webView = WKWebView()
+    private var urlString = ""
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        // webViewのサイズを指定
+        webView.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height)
+        view.addSubview(webView)
+
+        let urlForRequest = URL(string: urlString)
+        let request = URLRequest(url: urlForRequest!)
+        webView.load(request)
+    }
+
+    // ユーザー選択時にURLを受け取るためのメソッド
+    func passUserURLString(URLString: String) {
+        self.urlString = URLString
     }
 }
