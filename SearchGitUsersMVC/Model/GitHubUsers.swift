5c92241f159e45bba18de261e275c8077687b57e
sauna1137 <subscription.search@gmail.com>
2021-12-14 16:27:39 +0900

    GitHubUsers追加　first Commit
    GitHubのユーザーを表示するデータをまとめた構造体

---
+++ SearchGitUsersMVC/Model/GitHubUsers.swift
@@ -0,0 +1,15 @@
+//
+//  GitHubUsers.swift
+//  SearchGitUsersMVC
+//
+//  Created by KS on 2021/12/14.
+//
+
+import Foundation
+
+struct GitHubUsers {
+    let userName: String
+    let type: String
+    let imageURL: String
+    let userURL: String
+}
