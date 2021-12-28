6b4a8338d79cf3eea12db339754a3bae91e63ac6
sauna1137 <subscription.search@gmail.com>
2021-12-28 11:32:51 +0900

    新たな構造体の配列にせずitemsとしてそのまま渡す

--- SearchGitUsersMVC/Model/FetchGitHubUsers.swift
+++ SearchGitUsersMVC/Model/FetchGitHubUsers.swift
@@ -16,13 +16,9 @@ class FetchGitHubUsers {
 
     weak var fetchGitHubUsersDelegate: FetchGitHubUsersDelegate?
     private let gitHubAPIURL = "https://api.github.com/search/users?q=" // ユーザー情報を取得するAPI q=以下にキーワード
-    private var gitHubUsers = [GitHubUsers]()
-    private let maxContents = 30 // APIの構造上30件以上取得できないため
 
     // SearchVCで検索キーワードを元にAPIを叩いて値を返す
     func fetchGitHubUsers(searchText: String, completion: @escaping ([GitHubUsers]) -> Void) {
-
-        gitHubUsers.removeAll()
 
         AF.request(gitHubAPIURL + searchText) { _ in } .response { response in
 
@@ -33,28 +29,14 @@ class FetchGitHubUsers {
 
             do {
                 let json = try JSONDecoder().decode(CodableForGitHubUsers.self, from: data)
-                var totalCount = json.totalCount
+                let totalCount = json.totalCount
 
                 guard totalCount > 0 else {
                     self.fetchGitHubUsersDelegate?.presentAlert(content: "ユーザーは見つかりませんでした。")
                     return
                 }
 
-                if totalCount > self.maxContents {
-                    totalCount = self.maxContents
-                }
-
-                for user in 0..<totalCount {
-                    let userName = json.items[user].login
-                    let userType = json.items[user].type
-                    let imageURL = json.items[user].avatarURL
-                    let userURL = json.items[user].htmlURL
-
-                    let newGitHubUser = GitHubUsers(userName: userName, type: userType, imageURL: imageURL, userURL: userURL)
-                    self.gitHubUsers.append(newGitHubUser)
-                }
-
-                completion(self.gitHubUsers)
+                completion(json.items)
 
             } catch {
                 print(error.localizedDescription)
